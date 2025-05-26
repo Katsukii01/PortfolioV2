649e938eaa3fac677ab9345550b196f45c056c9a
Shad0w <kejkej120@gmail.com>
2025-05-26 22:16:45 +0200

    portfoliov3.002

--- src/components/index.js
+++ src/components/index.js
@@ -6,6 +6,6 @@ import Skills from "./Skills";
 import Technologies from "./Technologies";
 import About from "./About";
 import Experience from "./Experience";
-import Rain from "./rain";
+import Rain from "./Rain";
 
 export { Contact, Hero, Navbar, Projects, Skills, Technologies, About, Experience, Rain}
